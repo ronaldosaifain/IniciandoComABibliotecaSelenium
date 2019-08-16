a6f2963ab60813db8a2135c3288190c8637534b2
Ronaldo Santos <ronaldo100timo@gmail.com>
2019-08-16 16:15:53 -0300

    Listando e salvando em arquivo a descrição dos produtos

--- src/main/java/myfirstteste/firstteste/App.java
+++ src/main/java/myfirstteste/firstteste/App.java
@@ -1,28 +1,58 @@
 package myfirstteste.firstteste;
 
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.setProperty("webdriver.chrome.driver","C:\\drive\\chromedriver.exe");
-    
-       WebDriver navegador = new ChromeDriver();
-       navegador.manage().timeouts().implicitlyWait(20, TimeUnit.HOURS);
-       
-       navegador.get("https:\\www.amazon.com.br");
-       
-       navegador.findElement(By.name("field-keywords")).sendKeys("Iphone");
-       navegador.findElement(By.className("nav-input")).click();
-       
-       
-        System.out.println(navegador.findElement(By.className("a-size-medium a-color-base a-text-normal")).getText());
-       
-    }
+public class App {
+	@Test
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\drive\\chromedriver.exe");
+
+		String path = "C:\\Users\\Samsung\\Desktop\\text.xls";
+
+		WebDriver navegador = new ChromeDriver();
+
+		navegador.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+		navegador.get("https://www.amazon.com.br");
+
+		navegador.findElement(By.name("field-keywords")).sendKeys("Iphone");
+		navegador.findElement(By.className("nav-input")).click();
+
+		List<String> listaIphones = new ArrayList<String>();
+
+		Integer i = 0;
+
+		for (i = 1; i < 17; i++) {
+			String buttonXpath = " //*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + i
+					+ "]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span";
+
+			String Iphone = navegador.findElement(By.xpath(buttonXpath)).getText();
+
+			listaIphones.add(Iphone + "\n");
+
+		}
+
+		System.out.println(listaIphones + "\n");
+		escritor(path, listaIphones);
+
+	}
+
+	public static void escritor(String path, List<String> list) throws IOException {
+		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
+
+		buffWrite.append(list + "\n");
+		buffWrite.close();
+	}
+
+	
 }
