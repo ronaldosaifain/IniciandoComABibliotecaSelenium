1afe6ce29bf941c6498fea62fe56393f77ffbbc6
Ronaldo Santos <ronaldo100timo@gmail.com>
2019-08-16 17:26:18 -0300

    Listando os preços

--- src/main/java/myfirstteste/firstteste/App.java
+++ src/main/java/myfirstteste/firstteste/App.java
@@ -28,31 +28,41 @@ public class App {
 		navegador.findElement(By.name("field-keywords")).sendKeys("Iphone");
 		navegador.findElement(By.className("nav-input")).click();
 
-		List<String> listaIphones = new ArrayList<String>();
+		List<String> listaDescricao = new ArrayList<String>();
+		List<String> listaPreco = new ArrayList<String>();
 
-		Integer i = 0;
+		for (Integer i = 1; i < 17; i++) {
+			for (Integer j = 1; j < 9; j++) {
+				String XpathDescricao = " //*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + i
+						+ "]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span";
 
-		for (i = 1; i < 17; i++) {
-			String buttonXpath = " //*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + i
-					+ "]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span";
+				String XpathPreco = "//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + j
+						+ "]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]\r\n"
+						+ "";
 
-			String Iphone = navegador.findElement(By.xpath(buttonXpath)).getText();
+				String IphoneDescricao = navegador.findElement(By.xpath(XpathDescricao)).getText();
+				String IphonePreco = navegador.findElement(By.xpath(XpathPreco)).getText();
 
-			listaIphones.add(Iphone + "\n");
+				listaDescricao.add(IphoneDescricao + "\n");
+				listaPreco.add(IphonePreco + "\n");
 
+			}
 		}
 
-		System.out.println(listaIphones + "\n");
-		escritor(path, listaIphones);
+		System.out.println(listaDescricao + "\n" + listaPreco);
+		escritor(path, listaDescricao, listaPreco);
 
 	}
 
-	public static void escritor(String path, List<String> list) throws IOException {
+	public static void escritor(String path, List<String> list1,List<String> list2) throws IOException {
 		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
 
-		buffWrite.append(list + "\n");
+		
+		buffWrite.append(list1 + "/");
+		buffWrite.append(list2 + "/");
 		buffWrite.close();
 	}
 
-	
+	// *[@id="search"]/div[1]/div[2]/div/span[3]/div[1]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]
+	// *[@id="search"]/div[1]/div[2]/div/span[3]/div[1]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span/span[2]/span[2]
 }
