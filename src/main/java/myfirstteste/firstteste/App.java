d304e4bdc1d7a8e593ff68186cc54277e921082a
Ronaldo Santos <ronaldo100timo@gmail.com>
2019-08-17 00:37:01 -0300

    Associando cada preço a sua determinada descrição

--- src/main/java/myfirstteste/firstteste/App.java
+++ src/main/java/myfirstteste/firstteste/App.java
@@ -4,20 +4,21 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
-	@Test
+
 	public static void main(String[] args) throws IOException {
 		System.setProperty("webdriver.chrome.driver", "C:\\drive\\chromedriver.exe");
 
-		String path = "C:\\Users\\Samsung\\Desktop\\text.xls";
+		String path = "C:\\Users\\Samsung\\Desktop\\text.csv";
 
 		WebDriver navegador = new ChromeDriver();
 
@@ -29,40 +30,44 @@ public class App {
 		navegador.findElement(By.className("nav-input")).click();
 
 		List<String> listaDescricao = new ArrayList<String>();
-		List<String> listaPreco = new ArrayList<String>();
+		
 
-		for (Integer i = 1; i < 17; i++) {
-			for (Integer j = 1; j < 9; j++) {
-				String XpathDescricao = " //*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + i
-						+ "]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span";
+		for (Integer i = 1; i <= 15; i++) {
 
-				String XpathPreco = "//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + j
-						+ "]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]\r\n"
-						+ "";
+			if (i != 10 && i != 12) {
+				
+			String XpathDescricao = " //*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + i
+					+ "]/div/div/div/div[2]/div[2]/div/div[1]/div/div/div[1]/h2/a/span";
 
-				String IphoneDescricao = navegador.findElement(By.xpath(XpathDescricao)).getText();
-				String IphonePreco = navegador.findElement(By.xpath(XpathPreco)).getText();
+			String XpathPreco = "//*[@id=\"search\"]/div[1]/div[2]/div/span[3]/div[1]/div[" + i
+					+ "]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]\r\n"
+					+ "";
 
-				listaDescricao.add(IphoneDescricao + "\n");
-				listaPreco.add(IphonePreco + "\n");
+			String IphoneDescricao = navegador.findElement(By.xpath(XpathDescricao)).getText();
+			String IphonePreco = navegador.findElement(By.xpath(XpathPreco)).getText();
 
-			}
+			listaDescricao.addAll(Arrays.asList("Descricao: " + IphoneDescricao, "Preco: " + IphonePreco + "\n"));
+			
+			} else 
+				continue;
+
 		}
 
-		System.out.println(listaDescricao + "\n" + listaPreco);
-		escritor(path, listaDescricao, listaPreco);
-
+		System.out.println(listaDescricao);
+        
+		escreverArquivo(path, listaDescricao);
 	}
 
-	public static void escritor(String path, List<String> list1,List<String> list2) throws IOException {
+	
+	public static void escreverArquivo(String path, List<String> list1) throws IOException {
 		BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path));
 
-		
-		buffWrite.append(list1 + "/");
-		buffWrite.append(list2 + "/");
+		buffWrite.append(list1 + ",");
+
 		buffWrite.close();
 	}
+	
+	
 
-	// *[@id="search"]/div[1]/div[2]/div/span[3]/div[1]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span[1]/span[2]/span[2]
-	// *[@id="search"]/div[1]/div[2]/div/span[3]/div[1]/div[1]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div/div/a/span/span[2]/span[2]
+	
 }
